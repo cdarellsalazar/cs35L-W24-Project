ee5a86aad8911191ca73129cbfd5b4162c5395bc
Christian Salazar <97498157+cdarellsalazar@users.noreply.github.com>
2024-03-05 01:48:48 -0800

    coded the left hand side to show conversations; almost finished with it but I wanted to commit what I have so far on a different branch

--- frontend/src/pages/messaging.js
+++ frontend/src/pages/messaging.js
@@ -1,13 +1,110 @@
-import React, { useState } from "react";
-import NewConvo from "../components/NewConvo"
+import React, { useState, Component } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus, faEllipsis, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import "./messaging.css";
+import ConvoItems from "./conversationItems";
+
+//import { fa-plus } from "@fortawesome/free-solid-svg-icons";
+//import NewConvo from "../components/NewConvo"
 //import { Link } from 'react-router-dom';
 //uncomment this import when sign-out button is added
 
+
+export class Conversation extends Component {
+    allChatUsers = [
+        {
+          image:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTA78Na63ws7B7EAWYgTr9BxhX_Z8oLa1nvOA&usqp=CAU",
+          id: 1,
+          name: "Tim Hover",
+          active: true,
+          isOnline: true,
+        },
+        {
+          image:
+            "https://pbs.twimg.com/profile_images/1055263632861343745/vIqzOHXj.jpg",
+          id: 2,
+          name: "Ayub Rossi",
+          active: false,
+          isOnline: false,
+        },
+        {
+          image:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTQEZrATmgHOi5ls0YCCQBTkocia_atSw0X-Q&usqp=CAU",
+          id: 3,
+          name: "Hamaad Dejesus",
+          active: false,
+          isOnline: false,
+        },
+        {
+          image:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRZ6tM7Nj72bWjr_8IQ37Apr2lJup_pxX_uZA&usqp=CAU",
+          id: 4,
+          name: "Eleni Hobbs",
+          active: false,
+          isOnline: true,
+        },
+        {
+          image:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRJo1MiPQp3IIdp54vvRDXlhbqlhXW9v1v6kw&usqp=CAU",
+          id: 5,
+          name: "Elsa Black",
+          active: false,
+          isOnline: false,
+        },
+    ];
+
+    constructor(props) {
+        super(props);
+        this.state = {
+          allChats: this.allChatUsers,
+        };
+      }
+      render() {
+        return (
+            <div className="left-column">
+            <button className="btn">
+                <FontAwesomeIcon id="plus-sign" icon={faPlus}/>
+                <span>Start New Chat</span>
+            </button>
+            <div className="convo-sidebar-heading">
+                <h2>Conversations</h2>
+                <button className="btn-nobg">
+                    <FontAwesomeIcon icon={faEllipsis}/>
+                </button>
+            </div>
+            <div className="convo-search-bar">
+                <div className="search-wrap">
+                    <input type="text" placeholder="Search Conversations" required />
+                    <button className="search-btn">
+                        <FontAwesomeIcon icon={faMagnifyingGlass}/>
+                    </button>
+                </div>
+            </div>
+            <div className="conversations">
+              {this.state.allChats.map((item, index) => {
+                return (
+                  <ConvoItems
+                    name={item.name}
+                    key={item.id}
+                    animationDelay={index + 1}
+                    active={item.active ? "active" : ""}
+                    isOnline={item.isOnline ? "active" : ""}
+                    image={item.image}
+                  />
+                );
+              })}
+            </div>
+          </div>
+        );
+      }
+    }
 
 function Messaging() {
 
     const [message, setMessage] = useState('');
     const [showNewConversationBox, setShowNewConversationBox] = useState(false);
+    const[allChats, setAllChats] = useState([]);
 
     const handleInputChange = (event) => {
         setMessage(event.target.value);
@@ -51,14 +148,47 @@ function Messaging() {
                                 <button onClick={handleStartConversation}>Start Conversation</button>
                             </div>
     */
-    
+    //start convo button = btn
+    //ellipses = btn-nobg
     return (
         <div className="container">
             <div className="left-column">
-                Friends List Selection and Online Statuses Displayed
+                <button className="btn">
+                    <FontAwesomeIcon id="plus-sign" icon={faPlus}/>
+                    <span>Start New Chat</span>
+                </button>
+                <div className="convo-sidebar-heading">
+                    <h2>Conversations</h2>
+                    <button className="btn-nobg">
+                        <FontAwesomeIcon icon={faEllipsis}/>
+                    </button>
+                </div>
+                <div className="convo-search-bar">
+                    <div className="search-wrap">
+                        <input type="text" placeholder="Search Conversations" required />
+                        <button className="search-btn">
+                            <FontAwesomeIcon icon={faMagnifyingGlass}/>
+                        </button>
+                    </div>
+                </div>
+                <div className="conversations">
+                    {allChats.map((item, index) => {
+                        return (
+                            <ConvoItems
+                                name = {item.name}
+                                key = {item.id}
+                                animationDelay={index + 1}
+                                active={item.active ? "active" : ""}
+                                isOnline={item.isOnline ? "active" : ""}
+                                image={item.image} />
+                        );
+                    })}
                 
-                <button className="start-convo-button" onClick={handleNewConvo}>Create New Convo</button>
-                {showNewConversationBox && <NewConvo/>}
+
+                </div>
+                
+                {/*<button className="start-convo-button" onClick={handleNewConvo}>Create New Convo</button>
+                {showNewConversationBox && <NewConvo/>}*/}
 
                 
             </div>
