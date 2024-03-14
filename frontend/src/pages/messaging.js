40b867161f394800d0c4b6ce99d20ec6888c68ba
Christian Salazar <97498157+cdarellsalazar@users.noreply.github.com>
2024-03-14 13:21:18 -0700

    styling changes

--- frontend/src/pages/messaging.js
+++ frontend/src/pages/messaging.js
@@ -12,6 +12,7 @@ import Question from '../components/Question';
 import getUserByUserName from "../hooks/fetchUserByUsername";
 import NewConvo from "../components/NewConvo";
 import ProfileCard from "../components/ProfileCard";
+import '../components/NewConvo.css';
 
 function Messaging() {
     const now = new Date();
@@ -108,37 +109,8 @@ function Messaging() {
     function toggleBoolNo() {
         answer(!answered)
       }
-    function getCurrentDate() {
-        const today = new Date();
-        const year = today.getFullYear();
-        const month = String(today.getMonth() + 1).padStart(2, '0'); 
-        const day = String(today.getDate()).padStart(2, '0');
-        return `${year}-${month}-${day}`;
-    }
-    const handleSendMessage = () => {
-        fetch('/api/messages/send', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            }, 
-            body: JSON.stringify({
-                senderId: 'senderId',
-                receiverId: 'receiverId',
-                content:message
-            })
-        })
-        .then(response => response.json())
-        .then(data => {
-            console.log('Message sent:', data);
-            setMessage('');
-        })
-        .catch((error) => {
-            console.error('Error sending message:', error);
-        });
-    };
 
     const [currentConvoMessages, setCurrentConvoMessages] = useState([])
-    const newMessage = "";
     const [selectedConversation, setSelectedConversation] = useState();
     const [previousConversation, setPreviousConversation] = useState(null);
 
@@ -174,46 +146,6 @@ function Messaging() {
 
     }, [selectedConversation,currentConvoMessages])
 
-    /**const [currentConvoMessages, setCurrentConvoMessages] = useState( [
-        {
-          messageId: 8,
-          sender: "Paul Eggert",
-          receiver: "User Logged In",
-          msg: "How's your Latin?",
-          timeSent: "12:53",
-        },
-        {
-          messageId: 9,
-          sender: "User Logged In",
-          receiver: "Paul Eggert",
-          msg: "LOL",
-          timeSent: "12:54",
-        },
-        {
-          messageId: 10,
-          sender: "Paul Eggert",
-          receiver: "User Logged In",
-          msg: "I can't wait for everyone to fail the final!",
-          timeSent: "12:55",
-        },
-      ])**/
-      
-      //const [newMessage, setNewMessage] = useState('');
-      /*
-      useEffect(() => {
-        if (newMessage !== "") {
-          setCurrentConvoMessages(prevCurrentConvoMessages => [...prevCurrentConvoMessages, newMessage]);
-        }
-      }, [newMessage]);*/ // This effect runs whenever newMessage changes
-
-      //setCurrentConvoMessages(prevCurrentConvoMessages => [...prevCurrentConvoMessages, newMessage]);
-/*
-    useEffect(() => {
-        if (previousConversation) {
-            console.log("printing from useEffect");
-            console.log("Prev Selected Convo:", previousConversation.name);
-        }
-    }, [previousConversation]);*/
 
     const handleConversationClick = (newConversation) => {
         newConversation.selected = true;
@@ -226,33 +158,8 @@ function Messaging() {
         console.log("Selected Conversation:", newConversation.name);
         console.log("Messages of Selected Convo:", newConversation.messages);
     };
-    /*
-        const handleConversationClick = (newConversation) => {
-        // Set the clicked conversation's selected field to be true
-        newConversation.selected = true;
-        if(previousConversation === null) // if there isn't a previous conversation
-        {
-            setPreviousConversation(newConversation); // set it to the new conversation
-            console.log("Prev Selected Convo:", previousConversation); 
-        }
-        else // if there is a previous conversation
-        {
-            previousConversation.selected = false; // set its selected field to false
-            setPreviousConversation(newConversation); // set the previous conversation to the new conversation
-            console.log("Prev Selected Convo:", previousConversation.name);
-        }
-        
-        // Set the 'selected' field of the new conversation to true
-        const updatedNewConversation = { ...newConversation, selected: true };
-      
-        // Update the selectedConversation state with the new conversation
-        setSelectedConversation(updatedNewConversation);
-      
-        console.log("Selected Conversation:", updatedNewConversation.name);
+  
 
-      };*/
-
-    const [username, setUsername] = useState(null);
 
     const onNewChatSubmit = (newMessage) => {
         // Update the state with the new message
@@ -277,11 +184,10 @@ function Messaging() {
                           className="search-input"
                       />
                       <button onClick={handleSearch} className="search-button">Search</button>
-                  </div>
-                <div className="logo-container">
-                    <img src={logoImg} alt="Logo" style={{ maxWidth: '100px', maxHeight: '100px' }} />
-                </div>
-
+                      </div>
+                    <div className="logo-container">
+                        <img src={logoImg} alt="Logo" style={{ maxWidth: '100px', maxHeight: '100px' }} />
+                    </div>
                 <div className="disrupt-container">
                 {answered ? <Question toggleBoolYes={toggleBoolYes} toggleBoolNo={toggleBoolNo} /> : <Answered />}
                 </div>
